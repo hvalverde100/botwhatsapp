d7a706b20c74cfe5ca98eeb2884d9ab358c4da8d
hvalverde100 <hvalverde60@gmail.com>
2025-06-06 00:05:58 -0600

    Add full webhook debug + ChatGPT logic

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ def webhook():
 </Response>"""
     return twiml_response, 200, {'Content-Type': 'application/xml'}
 
-
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
